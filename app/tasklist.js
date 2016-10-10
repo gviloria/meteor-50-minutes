1e422934951f7ae82b4f9cb1d704b2b7264cedb1
Gabriel L. Viloria <gviloria@hawaii.edu>
2016-10-09 22:51:46 -1000

    Task 2: Tasklist Setup

---
+++ app/tasklist.js
@@ -0,0 +1,16 @@
+/**
+ * Created by gviloria on 10/9/16.
+ */
+Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isClient) {
+  Template.tasks.helpers({
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+
+}
